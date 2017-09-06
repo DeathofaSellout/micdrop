2b0120963b065f7ff3af3758c73a6dfb9c36906e
balbini <balbini91@gmail.com>
2017-09-06 11:54:33 -0700

    server setup completed

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const app = express(); //allowing us to use express
 // Mount router middleware to the application middleware
 app.use(require("./resources"));
 
+// Request Promise
+const request = require("request-promise");
+
+// Body Parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Method Override
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+// Static assets
+app.use(express.static("./assets"));
+
 // Preparing port for Heroku deployment
 const port = process.env.PORT || 3000;
 

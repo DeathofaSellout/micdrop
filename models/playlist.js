c5509f99409df820aff483b84a65c455cc91967b
deathofasellout <javascriptforlife@gmail.com>
2017-09-07 16:21:49 -0700

    index.js update

--- models/playlist.js
+++ models/playlist.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //Connect Mongoose to MongoDB
-mongoose.connect("mongodb://localhost/playlist_db"); //What does the stuff in parentheses mean? Research MongoDB
+mongoose.connect("mongodb://localhost/3000"); //What does the stuff in parentheses mean? Research MongoDB
 
 //Create a new schema to model our "playlist" data
 const PlaylistSchema = new Schema({
